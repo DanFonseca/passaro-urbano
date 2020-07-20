d82926c858cbe817245955691a10d31c47b75882
danie <daniel.freitas.ms1@gmail.com>
2020-07-19 22:32:56 -0300

    primeiro commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {OfertaService} from '../../ofertas.service';
+import {Oferta} from '../oferta.model';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers : [OfertaService]
+})
+export class HomeComponent implements OnInit {
+
+  public ofertas : Oferta[];
+
+  constructor(public oferta : OfertaService) { }
+
+  ngOnInit(): void {
+      this.oferta.getOfertasPromisses()
+                  .then ( (conteudo : Oferta [])  => {
+                    this.ofertas = conteudo
+                  })
+  }
+}
